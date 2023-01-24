01617b3da76820ab9f3f4b0da90c5e0301242876
Frank Calise <fcalise@gmail.com>
2023-01-24 08:50:04 -0500

    added ignite boilerplate

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,15 @@
+const plugins = [
+  [
+    '@babel/plugin-proposal-decorators',
+    {
+      legacy: true,
+    },
+  ],
+  ['@babel/plugin-proposal-optional-catch-binding'],
+  'react-native-reanimated/plugin', // NOTE: this must be last in the plugins
+];
+
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins,
 };
--- index.js
+++ index.js
@@ -3,7 +3,16 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
 import {name as appName} from './app.json';
+
+import App from './app/app.tsx';
+import React from 'react';
+import {AppRegistry} from 'react-native';
+// import RNBootSplash from 'react-native-bootsplash';
+
+function IgniteApp() {
+  // return <App hideSplashScreen={RNBootSplash.hide} />;
+  return <App />;
+}
 
 AppRegistry.registerComponent(appName, () => App);
